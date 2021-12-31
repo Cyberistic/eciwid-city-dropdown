bed84a51284a63aeb05e2bfd658010f11857e8d9
Cyberistic <Cyb3ristic@gmail.com>
2021-12-31 09:46:12 +0300

    .

--- dropdown.js
+++ dropdown.js
@@ -57,14 +57,14 @@ for (const city in cities) {
 // };
 
 function checkout_change() {
-    document.getElementById("ec-country").addEventListener('DOMContentLoaded', function(event) {
+    ec.document.getElementById("ec-country").addEventListener('DOMContentLoaded', function(event) {
         
-        var countryselect=document.getElementById("ec-country");
+        var countryselect=ec.document.getElementById("ec-country");
         countryselect.options[0]=new Option("republic of cd", "CD",true,true); //replace 1st option with a new one
         console.log(countryselect.options[0])
         countryselect.options[62].selected = true;
         countryselect.options[62].defaultSelected = true;
-        document.getElementsByClassName('ec-form__cell--country')[0].style.display = 'none';
+        ec.document.getElementsByClassName('ec-form__cell--country')[0].style.display = 'none';
       })
 
     }
