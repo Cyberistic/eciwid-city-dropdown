1672c4f002757763d4ff00d7f9663257ba9ba258
Cyberistic <Cyb3ristic@gmail.com>
2021-12-31 07:11:59 +0300

    .

--- dropdown.js
+++ dropdown.js
@@ -5,34 +5,52 @@ ec.order = ec.order || {};
 ec.order.extraFields = ec.order.extraFields || {};
 
 // The field "how_did_you_find_us" asks user about how they found the store. Drop down type
-ec.order.extraFields.how_did_you_find_us = {
-    'title': 'How did you find us?',
-    'type': 'select',
-    'required': false,
-    // 'selectOptions': ['Google Ads', 'Friend told me', 'TV show', 'Other'],
-    'options': [
-        {'title': 'Google Ads'},
-      {'title': 'Friend told me'},
-      {'title': 'TV show'},
-      {'title': 'Other'}
-    ],
-    'value': 'TV show', // Default value
-    'checkoutDisplaySection': 'shipping_address'
-};
 
-// Add pickup time selection for customer
-ec.order.extraFields.ecwid_pickup_time = {
-    'title': '_msg_ShippingDetails.pickup.customer_header',
-    'required': true,
-    'type': 'datetime',
-    'checkoutDisplaySection': 'shipping_address',
-    'orderDetailsDisplaySection': 'shipping_info',
+const cities = {
+    'AH': ['City a', 'City b'],
+    'HA': ['City c', 'City e']
 }
 
-// Hidden field, which is not shown at checkout
-ec.order.extraFields.my_custom_field = {
-    'value': 'abcd12345'
-};
+for (const city in cities) {
+    ec.order.extraFields[city] = {
+        'title': '',
+        'type': 'select',
+        'required': false,
+        // 'selectOptions': ['Google Ads', 'Friend told me', 'TV show', 'Other'],
+        'options': cities[city],
+        'value': 'TV show', // Default value
+        'checkoutDisplaySection': 'shipping_address'
+    }
+}
+// ec.order.extraFields.how_did_you_find_us = {
+//     'title': 'How did you find us?',
+//     'type': 'select',
+//     'required': false,
+//     // 'selectOptions': ['Google Ads', 'Friend told me', 'TV show', 'Other'],
+//     'options': [
+//         {'title': 'Google Ads'},
+//       {'title': 'Friend told me'},
+//       {'title': 'TV show'},
+//       {'title': 'Other'}
+//     ],
+//     'value': 'TV show', // Default value
+//     'checkoutDisplaySection': 'shipping_address'
+// };
+
+
+// // Add pickup time selection for customer
+// ec.order.extraFields.ecwid_pickup_time = {
+//     'title': '_msg_ShippingDetails.pickup.customer_header',
+//     'required': true,
+//     'type': 'datetime',
+//     'checkoutDisplaySection': 'shipping_address',
+//     'orderDetailsDisplaySection': 'shipping_info',
+// }
+
+// // Hidden field, which is not shown at checkout
+// ec.order.extraFields.my_custom_field = {
+//     'value': 'abcd12345'
+// };
 
 function checkout_change() {
     document.getElementById("ec-country").addEventListener('DOMContentLoaded', function(event) {
